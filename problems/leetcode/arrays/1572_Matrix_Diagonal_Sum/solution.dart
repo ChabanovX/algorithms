3ccce011e22f074515492679f01aa0f1e69bbead
ChabanovX <firstchabanov@gmail.com>
2025-07-27 15:10:27 +0300

    Slightly change 1572

--- problems/leetcode/arrays/1572_Matrix_Diagonal_Sum/solution.dart
+++ problems/leetcode/arrays/1572_Matrix_Diagonal_Sum/solution.dart
@@ -1,16 +1,15 @@
 class Solution {
   int diagonalSum(List<List<int>> mat) {
-    final int middleIndex = (mat.length / 2).floor();
-    final bool isOddLen = mat.length % 2 == 1;
-
     int twoSum = 0;
 
     for (int i = 0; i < mat.length; i++) {
       twoSum += mat[i][i];
+      twoSum += mat[i][mat.length - 1 - i];
+    }
 
-      if (!(i == middleIndex && isOddLen)) {
-        twoSum += mat[i][mat.length - 1 - i];
-      }
+    if (mat.length % 2 == 1) {
+      final middle = (mat.length / 2).floor();
+      twoSum -= mat[middle][middle];
     }
 
     return twoSum;
